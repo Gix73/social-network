7d7f7f6cb74b437931bb1fd28512e764ddf4e060
Gix73 <gix73@yandex.ru>
2022-02-20 16:38:45 +0300

    feat: add messages page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,19 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + " " + s.active}>Dima</div>
+                <div className={s.dialog}>Kostya</div>
+                <div className={s.dialog}>Ivan</div>
+                <div className={s.dialog}>Max</div>
+                <div className={s.dialog}>Vlad</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Fine</div>
+            </div>
         </div>
     );
 }
